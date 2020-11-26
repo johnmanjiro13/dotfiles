62c1397743d91da9a844d1ba55415dd185231731
johnmanjiro13 <juntama193@gmail.com>
2020-11-26 14:27:15 +0900

    use gitmux

--- tmux/__deploy__.sh
+++ tmux/__deploy__.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ln -sf "$DOTPATH"/tmux/tmux.conf ~/.tmux.conf
+ln -sf "$DOTPATH"/tmux/gitmux.conf ~/.tmux/gitmux.conf
 
 ln -sf "DOTPATH"/tmux/sh/wifi.sh /usr/local/bin/wifi
 ln -sf "DOTPATH"/tmux/sh/battery.sh /usr/local/bin/battery
