db4e2f76b86576c3719e2a9f0cd52abd6a1b92d6
johnmanjiro13 <juntama193@gmail.com>
2021-07-27 07:44:10 +0900

    clippy with rustup

--- cargo/__deploy__.sh
+++ cargo/__deploy__.sh
@@ -4,8 +4,14 @@ mkdir -p ~/.cargo
 
 ln -sf "$DOTPATH"/cargo/config ~/.cargo/config
 
-PACKAGES="${DOTPATH}/packages/cargo.txt"; readonly PACKAGES
+PACKAGES="${DOTPATH}/packages/cargo.txt"
 
 for package in `cat "$PACKAGES"`; do
   cargo install "$package"
+done
+
+PACKAGES="${DOTPATH}/packages/rustup.txt"
+
+for package in `cat "$PACKAGES"`; do
+  rustup component add clippy
 done
