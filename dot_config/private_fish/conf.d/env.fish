2d9f7c50c84907620b151f768f7d85dabe031cce
johnmanjiro13 <28798279+johnmanjiro13@users.noreply.github.com>
2023-02-05 14:28:19 +0900

    fish envs

---
+++ dot_config/private_fish/conf.d/env.fish
@@ -0,0 +1,36 @@
+set -x LANG ja_JP.UTF-8
+set -x CLICOLOR 1
+
+# PATH
+set -x PATH $HOME/bin $PATH
+set -x PATH $HOME/.local/bin $PATH
+
+# XDG
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_CACHE_HOME $HOME/.cache
+
+# go
+if [ (command -v go )]
+  set -x GOPATH $HOME/go
+  set -x PATH $GOPATH/bin $PATH
+end
+
+# anyenv
+if [ (command -v anyenv) ]
+  set -x ANYENV_DEFINITION_ROOT $XDG_CONFIG_HOME/anyenv/anyenv-install
+end
+
+# docker
+set -x DOCKER_BUILDKIT 1
+
+# Homebrew
+set -x HOMEBREW_NO_AUTO_UPDATE 1
+if [ (command arch) = "arm64" ]
+  set -x PATH /opt/homebrew/bin $PATH
+end
+
+# Rust
+if [ (command -v cargo) ]
+  set -x PATH $HOME/.cargo/bin $PATH
+end
