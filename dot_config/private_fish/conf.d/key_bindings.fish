56b4487fd7832252dff90d35bbe122e2ee0b65ca
johnmanjiro13 <28798279+johnmanjiro13@users.noreply.github.com>
2023-02-05 17:30:41 +0900

    install fish plugins

---
+++ dot_config/private_fish/conf.d/key_bindings.fish
@@ -0,0 +1,6 @@
+bind \cg\cg '__ghq_repository_search'
+if bind -M insert >/dev/null 2>/dev/null
+    bind -M insert \cg\cg '__ghq_repository_search'
+end
+
+fzf_configure_bindings --git_log=\cg\cl --git_status=\cg\cs --process=\cp
